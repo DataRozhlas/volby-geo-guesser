a09ce6f43c2396d6a71b0e919a6f4b4766df794b
Petr Kočí <petr.koci@samizdat.cz>
2021-06-22 20:46:00 +0200

    debug mode off

--- js/script.js
+++ js/script.js
@@ -9,37 +9,37 @@ import useScript from "./useScript";
 import useMapLoader from "./useMapLoader";
 
 function App() {
-  // const [currPlace, setCurrPlace] = useState(
-  //   data[Math.floor(Math.random() * data.length)]
-  // );
-  // const [guessedPlaces, setGuessedPlaces] = useState([]);
-  // const [guessedResults, setGuessedResults] = useState([]);
-  const [currPlace, setCurrPlace] = useState({
-    id: 371,
-    str: 24,
-    hl: 29,
-    hlclk: 72,
-    obc: "Janov",
-    X: 16.2518,
-    Y: 50.3323,
-    okr: 1,
-    okres: 5204,
-  });
-  const [guessedPlaces, setGuessedPlaces] = useState([
-    1, 1, 1, 1, 1, 1, 1, 1, 1, 371,
-  ]);
-  const [guessedResults, setGuessedResults] = useState([
-    true,
-    true,
-    true,
-    false,
-    false,
-    false,
-    true,
-    true,
-    true,
-    false,
-  ]);
+  const [currPlace, setCurrPlace] = useState(
+    data[Math.floor(Math.random() * data.length)]
+  );
+  const [guessedPlaces, setGuessedPlaces] = useState([]);
+  const [guessedResults, setGuessedResults] = useState([]);
+  // const [currPlace, setCurrPlace] = useState({
+  //   id: 371,
+  //   str: 24,
+  //   hl: 29,
+  //   hlclk: 72,
+  //   obc: "Janov",
+  //   X: 16.2518,
+  //   Y: 50.3323,
+  //   okr: 1,
+  //   okres: 5204,
+  // });
+  // const [guessedPlaces, setGuessedPlaces] = useState([
+  //   1, 1, 1, 1, 1, 1, 1, 1, 1, 371,
+  // ]);
+  // const [guessedResults, setGuessedResults] = useState([
+  //   true,
+  //   true,
+  //   true,
+  //   false,
+  //   false,
+  //   false,
+  //   true,
+  //   true,
+  //   true,
+  //   false,
+  // ]);
   const [loaded, error] = useScript("https://api.mapy.cz/loader.js");
   const [mapLoader] = useMapLoader(loaded);
   return (
